52521a4857460f125c5f3038a53edff970e88d29
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-10-09 16:13:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,6 @@ if check_password():
             # Load data
             if st.button("Update"):
                 st.session_state.data, st.session_state.height_max = dataLoad(_conn=conn, idmin= idmin, idmax=idmax)
-                st.write(st.session_state.data.head())
             
             if 'data' in st.session_state:
                 st.write(str(st.session_state.data["ROUTE_NAME"][0])+ ", DFO: "+str(st.session_state.data["DFO"].min())+ "~"+ str(st.session_state.data["DFO"].max()))
